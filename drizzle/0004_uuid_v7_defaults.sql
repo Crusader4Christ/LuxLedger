698ae0e44c0d5625cfed07173cdca0043fa9ade5
Herman Kliushyn <h.kliushyn@enjoygaming.com>
2026-05-31 19:15:26 +0200

    LL-56 make uuid_v7 migration independent from pgcrypto

--- drizzle/0004_uuid_v7_defaults.sql
+++ drizzle/0004_uuid_v7_defaults.sql
@@ -7,10 +7,10 @@ AS $$
     SELECT lpad(to_hex(floor(extract(epoch from clock_timestamp()) * 1000)::bigint), 12, '0') AS v
   ),
   rb AS (
-    SELECT encode(gen_random_bytes(16), 'hex') AS v
+    SELECT md5(random()::text || clock_timestamp()::text || random()::text) AS v
   ),
   va AS (
-    SELECT substr('89ab', (get_byte(gen_random_bytes(1), 0) % 4) + 1, 1) AS v
+    SELECT substr('89ab', floor(random() * 4)::int + 1, 1) AS v
   )
   SELECT (
     substr(ts.v, 1, 8) || '-' ||
