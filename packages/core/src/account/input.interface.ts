0b54c4915143cd8b5542780b9f7a959cf0eb3c57
Herman Kliushyn <h.kliushyn@enjoygaming.com>
2026-07-30 23:56:21 +0200

    LL-82 expose nullable account code input

--- packages/core/src/account/input.interface.ts
+++ packages/core/src/account/input.interface.ts
@@ -3,7 +3,7 @@ import type { AccountSide, OverdraftPolicy } from './entity';
 export interface CreateAccountInput {
   tenantId: string;
   ledgerId: string;
-  code?: string;
+  code?: string | null;
   name: string;
   side: AccountSide;
   overdraftPolicy?: OverdraftPolicy;
